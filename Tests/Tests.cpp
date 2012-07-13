24a88e4309592edc30cbe04238d38c814869e4db
David Doria <daviddoria@gmail.com>
2012-07-13 11:53:09 -0400

    Add a test.

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -1,10 +1,14 @@
 #include "Helpers.h"
 
-void TestGetFileExtension();
+static void TestGetFileExtension();
+
+static void TestSortBySecondAccending();
 
 int main()
 {
-  TestGetFileExtension();
+  //TestGetFileExtension();
+
+  TestSortBySecondAccending();
   
   return 0;
 }
@@ -15,4 +19,14 @@ void TestGetFileExtension()
   std::string extension = Helpers::GetFileExtension(filename);
 
   std::cout << "extension: " << extension << std::endl;
+}
+
+void TestSortBySecondAccending()
+{
+  typedef std::pair<int, float> MyType;
+  std::vector<MyType> v{
+        {1, 4.5}, {3, 6.7}, {9, 7.8}
+  };
+
+  std::sort(v.begin(), v.end(), Helpers::SortBySecondAccending<MyType>);
 }

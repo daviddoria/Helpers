47e72401a5f04f0f0ae8092d80da0b119da89079
David Doria <daviddoria@gmail.com>
2012-11-18 10:07:00 -0500

    Add placeholders for tests.

--- Tests/TestHelpers.cpp
+++ Tests/TestHelpers.cpp
@@ -3,56 +3,222 @@
 // STL
 #include <sstream>
 
-static void TestGetFileExtension();
+static bool TestFuzzyCompare();
 
-static void TestSortBySecondAccending();
+static bool TestGetFileExtension();
 
-static void TestInlineIgnore();
+static bool TestMin();
+static bool TestMax();
 
-static void TestWeightedAverage();
+static bool TestSortBySecondAccending();
+static bool TestSortByFirstAccending();
 
-static void TestNormalizeVectorInPlace();
+static bool TestInlineIgnore();
 
-static void TestNormalizeVector();
+static bool TestWeightedAverage();
 
-static void TestMinOfIndex();
+static bool TestNormalizeVectorInPlace();
 
-static void TestMinOfAllIndices();
+static bool TestNormalizeVector();
 
-static void TestSphericalToCartesian();
-static void TestCartesianToSpherical();
+static bool TestMinOfIndex();
 
-static void TestHasBracketOperator();
+static bool TestMinOfAllIndices_Vector();
+static bool TestMinOfAllIndices_Scalar();
+
+static bool TestMaxOfAllIndices_Vector();
+static bool TestMaxOfAllIndices_Scalar();
+
+static bool TestSphericalToCartesian();
+static bool TestCartesianToSpherical();
+
+static bool TestHasBracketOperator();
+
+static bool TestNegativeLog();
+
+static bool TestGetPath();
+
+static bool TestReplaceFileExtension();
+
+static bool TestGetSequentialFileName();
+
+static bool TestSideLengthFromRadius();
+
+static bool TestIsOdd();
+
+static bool TestZeroPad();
+
+static bool TestStringsMatch();
+
+static bool TestRoundAwayFromZero();
+
+static bool TestRandomInt();
+
+static bool TestIsValidRGB();
+
+static bool TestArgmin();
+
+static bool TestArgmax();
+
+static bool TestMaxOfIndex();
+
+static bool TestVectorMedian();
+
+static bool TestConvertFrom();
+
+static bool TestSum();
+
+static bool TestVectorSumOfAbsoluteDifferences();
+
+static bool TestOutputNode();
+
+static bool TestWriteVectorToFile();
+
+static bool TestWriteVectorToFileLines();
+
+static bool TestOutputFirst();
+
+static bool TestExtractFirst();
+
+static bool TestContains();
+
+static bool TestOutput();
+
+static bool TestClosestIndex();
+
+static bool TestIsNaN();
+
+static bool TestContainsNaN();
+
+static bool TestKeepTopN();
+
+static bool TestDoesQueueContain();
+
+static bool TestDoesStackContain();
+
+static bool TestForce0to255();
+
+static bool TestHSV_H_Difference();
 
 int main()
 {
-  TestGetFileExtension();
+  bool AllTestsPass = false;
 
-  TestSortBySecondAccending();
+  AllTestsPass &= TestFuzzyCompare();
 
-  TestInlineIgnore();
+  AllTestsPass &= TestGetFileExtension();
 
-  TestWeightedAverage();
+  AllTestsPass &= TestSortBySecondAccending();
+  AllTestsPass &= TestSortByFirstAccending();
 
-  TestNormalizeVectorInPlace();
+  AllTestsPass &= TestInlineIgnore();
 
-  TestNormalizeVector();
+  AllTestsPass &= TestWeightedAverage();
 
-  TestMinOfIndex();
+  AllTestsPass &= TestNormalizeVectorInPlace();
 
-  TestMinOfAllIndices();
+  AllTestsPass &= TestNormalizeVector();
 
-  TestSphericalToCartesian();
-  TestCartesianToSpherical();
+  AllTestsPass &= TestMinOfIndex();
 
-  TestHasBracketOperator();
+  AllTestsPass &= TestMinOfAllIndices_Vector();
+  AllTestsPass &= TestMinOfAllIndices_Scalar();
 
-  return 0;
+  AllTestsPass &= TestMaxOfAllIndices_Vector();
+  AllTestsPass &= TestMaxOfAllIndices_Scalar();
+
+  AllTestsPass &= TestSphericalToCartesian();
+  AllTestsPass &= TestCartesianToSpherical();
+
+  AllTestsPass &= TestHasBracketOperator();
+
+  AllTestsPass &= TestNegativeLog();
+
+  AllTestsPass &= TestGetPath();
+
+  AllTestsPass &= TestReplaceFileExtension();
+
+  AllTestsPass &= TestGetSequentialFileName();
+
+  AllTestsPass &= TestSideLengthFromRadius();
+
+  AllTestsPass &= TestIsOdd();
+
+  AllTestsPass &= TestZeroPad();
+
+  AllTestsPass &= TestStringsMatch();
+
+  AllTestsPass &= TestRoundAwayFromZero();
+
+  AllTestsPass &= TestRandomInt();
+
+  AllTestsPass &= TestIsValidRGB();
+
+  AllTestsPass &= TestArgmin();
+
+  AllTestsPass &= TestArgmax();
+
+  AllTestsPass &= TestMaxOfIndex();
+
+  AllTestsPass &= TestMin();
+  AllTestsPass &= TestMax();
+
+  AllTestsPass &= TestVectorMedian();
+
+  AllTestsPass &= TestConvertFrom();
+
+  AllTestsPass &= TestSum();
+
+  AllTestsPass &= TestVectorSumOfAbsoluteDifferences();
+
+  AllTestsPass &= TestOutputNode();
+
+  AllTestsPass &= TestWriteVectorToFile();
+
+  AllTestsPass &= TestWriteVectorToFileLines();
+
+  AllTestsPass &= TestOutputFirst();
+
+  AllTestsPass &= TestExtractFirst();
+
+  AllTestsPass &= TestContains();
+
+  AllTestsPass &= TestOutput();
+
+  AllTestsPass &= TestClosestIndex();
+
+  AllTestsPass &= TestIsNaN();
+
+  AllTestsPass &= TestContainsNaN();
+
+  AllTestsPass &= TestKeepTopN();
+
+  AllTestsPass &= TestDoesQueueContain();
+
+  AllTestsPass &= TestDoesStackContain();
+
+  AllTestsPass &= TestForce0to255();
+
+  AllTestsPass &= TestHSV_H_Difference();
+
+  if(AllTestsPass)
+  {
+    return EXIT_SUCCESS;
+  }
+  else
+  {
+    std::cerr << "Not all tests passed!" << std::endl;
+    return EXIT_FAILURE;
+  }
 }
 
-void TestNormalizeVector()
+bool TestNormalizeVector()
 {
   std::cout << "TestNormalizeVector()" << std::endl;
+
+  bool allPass = true;
+
+  // Test with same type
   {
   std::vector<float> a;
   a.push_back(1);
@@ -60,8 +226,12 @@ void TestNormalizeVector()
   a.push_back(3);
 
   std::vector<float> normalized = Helpers::NormalizeVector(a);
+  // Octave: a=[1 2 3]; b=a/norm(a)
+  std::vector<float> correctAnswer = {0.26726, 0.53452, 0.80178};
+  allPass &= Helpers::FuzzyCompare(normalized, correctAnswer);
   }
 
+  // Test with different type
   {
   std::vector<int> a;
   a.push_back(1);
@@ -69,24 +239,31 @@ void TestNormalizeVector()
   a.push_back(3);
 
   std::vector<float> normalized = Helpers::NormalizeVector(a);
+  // Octave: a=[1 2 3]; b=a/norm(a)
+  std::vector<float> correctAnswer = {0.26726, 0.53452, 0.80178};
+  allPass &= Helpers::FuzzyCompare(normalized, correctAnswer);
   }
+
+  return allPass;
 }
 
-void TestNormalizeVectorInPlace()
+bool TestNormalizeVectorInPlace()
 {
   std::cout << "TestNormalizeVectorInPlace()" << std::endl;
-  {
+
   std::vector<float> a;
   a.push_back(1);
   a.push_back(2);
   a.push_back(3);
 
   Helpers::NormalizeVectorInPlace(a);
-  }
 
+  // Octave: a=[1 2 3]; b=a/norm(a)
+  std::vector<float> correctAnswer = {0.26726, 0.53452, 0.80178};
+  return Helpers::FuzzyCompare(a, correctAnswer);
 }
 
-void TestGetFileExtension()
+bool TestGetFileExtension()
 {
   std::cout << "TestGetFileExtension()" << std::endl;
   std::string filename = "test.txt";
@@ -95,7 +272,18 @@ void TestGetFileExtension()
   std::cout << "extension: " << extension << std::endl;
 }
 
-void TestSortBySecondAccending()
+bool TestSortByFirstAccending()
+{
+  std::cout << "TestSortByFirstAccending()" << std::endl;
+  typedef std::pair<int, float> MyType;
+  std::vector<MyType> v{
+        {1, 4.5}, {3, 6.7}, {9, 7.8}
+  };
+
+  std::sort(v.begin(), v.end(), Helpers::SortByFirstAccending<MyType>);
+}
+
+bool TestSortBySecondAccending()
 {
   std::cout << "TestSortBySecondAccending()" << std::endl;
   typedef std::pair<int, float> MyType;
@@ -106,7 +294,7 @@ void TestSortBySecondAccending()
   std::sort(v.begin(), v.end(), Helpers::SortBySecondAccending<MyType>);
 }
 
-void TestInlineIgnore()
+bool TestInlineIgnore()
 {
   std::cout << "TestInlineIgnore()" << std::endl;
   std::stringstream ss;
@@ -118,7 +306,7 @@ void TestInlineIgnore()
   std::cout << a << " " << b << std::endl;
 }
 
-void TestWeightedAverage()
+bool TestWeightedAverage()
 {
   std::cout << "TestWeightedAverage()" << std::endl;
   std::vector<int> values;
@@ -136,7 +324,7 @@ void TestWeightedAverage()
   std::cout << "WeightedAverage: " << weightedAverage << std::endl;
 }
 
-void TestMinOfIndex()
+bool TestMinOfIndex()
 {
   std::cout << "TestMinOfIndex()" << std::endl;
   std::vector<std::vector<int> > vectorOfVectors;
@@ -154,9 +342,55 @@ void TestMinOfIndex()
   std::cout << "minComponent2: " << minComponent2 << std::endl;
 }
 
-void TestMinOfAllIndices()
+
+bool TestMaxOfAllIndices_Vector()
 {
-  std::cout << "TestMinOfAllIndices()" << std::endl;
+  std::cout << "TestMaxOfAllIndices_Vector()" << std::endl;
+  const unsigned int numberOfComponents = 3;
+  typedef std::vector<int> VectorType;
+  std::vector<VectorType> vectorOfVectors;
+  for(unsigned int i = 4; i < 10; ++i)
+  {
+    VectorType v(numberOfComponents);
+    v[0] = i;
+    v[1] = i;
+    v[2] = i;
+    vectorOfVectors.push_back(v);
+  }
+
+  VectorType maxComponents(numberOfComponents);
+
+  Helpers::MaxOfAllIndices(vectorOfVectors, maxComponents);
+
+  std::cout << "maxComponents: ";
+  for(size_t i = 0; i < maxComponents.size(); ++i)
+  {
+    std::cout << maxComponents[i] << std::endl;
+  }
+}
+
+bool TestMaxOfAllIndices_Scalar()
+{
+  std::cout << "TestMaxOfAllIndices_Scalar()" << std::endl;
+
+  typedef std::vector<int> VectorType;
+
+  VectorType v;
+
+  for(unsigned int i = 4; i < 10; ++i)
+  {
+    v.push_back(i);
+  }
+
+  VectorType::value_type maxValue;
+  Helpers::MaxOfAllIndices(v, maxValue);
+
+  std::cout << "maxValue: " << maxValue << std::endl;
+}
+
+bool TestMinOfAllIndices_Vector()
+{
+  std::cout << "TestMinOfAllIndices_Vector()" << std::endl;
   const unsigned int numberOfComponents = 3;
   typedef std::vector<int> VectorType;
   std::vector<VectorType> vectorOfVectors;
@@ -180,7 +414,26 @@ void TestMinOfAllIndices()
   }
 }
 
-void TestSphericalToCartesian()
+bool TestMinOfAllIndices_Scalar()
+{
+  std::cout << "TestMinOfAllIndices_Scalar()" << std::endl;
+
+  typedef std::vector<int> VectorType;
+
+  VectorType v;
+
+  for(unsigned int i = 4; i < 10; ++i)
+  {
+    v.push_back(i);
+  }
+
+  VectorType::value_type minValue;
+  Helpers::MinOfAllIndices(v, minValue);
+
+  std::cout << "minValue: " << minValue << std::endl;
+}
+
+bool TestSphericalToCartesian()
 {
 //  std::cout << "TestSphericalToCartesian()" << std::endl;
 //  double r = 1.0;
@@ -195,7 +448,7 @@ void TestSphericalToCartesian()
 //            << " to Cartesian: x=" << x << " y=" << y << " z=" << z << std::endl;
 }
 
-void TestCartesianToSpherical()
+bool TestCartesianToSpherical()
 {
 //  std::cout << "TestCartesianToSpherical()" << std::endl;
 //  double r, theta, phi;
@@ -211,7 +464,7 @@ void TestCartesianToSpherical()
 
 }
 
-void TestHasBracketOperator()
+bool TestHasBracketOperator()
 {
 //  typedef std::vector<float> VectorType;
 //  static_assert(Helpers::HasBracketOperator<VectorType>::value,
@@ -220,4 +473,222 @@ void TestHasBracketOperator()
 //  typedef std::vector<int> VectorType;
 //  static_assert(Helpers::HasBracketOperator<VectorType>::value,
 //                "VectorType does not have an operator[]!");
+}
+
+
+bool TestNegativeLog()
+{
+  float negativeLog = Helpers::NegativeLog(1.0f);
+}
+
+bool TestGetPath()
+{
+
+}
+
+bool TestReplaceFileExtension()
+{
+
+}
+
+bool TestGetSequentialFileName()
+{
+
+}
+
+bool TestSideLengthFromRadius()
+{
+
+}
+
+bool TestIsOdd()
+{
+
+}
+
+bool TestZeroPad()
+{
+
+}
+
+bool TestStringsMatch()
+{
+  std::string myName = "David";
+  std::string myName2 = "David";
+  if(!Helpers::StringsMatch(myName, myName2))
+  {
+    throw std::runtime_error("StringsMatch failed!");
+  }
+}
+
+bool TestRoundAwayFromZero()
+{
+
+}
+
+bool TestRandomInt()
+{
+  int randomInt = Helpers::RandomInt(1,5);
+}
+
+
+bool TestIsValidRGB()
+{
+  throw std::runtime_error("TestIsValidRGB() Not yet implemented!");
+  return false;
+}
+
+bool TestArgmin()
+{
+  throw std::runtime_error("TestArgmin() Not yet implemented!");
+  return false;
+}
+
+
+bool TestArgmax()
+{
+  throw std::runtime_error("TestArgmax() Not yet implemented!");
+  return false;
+}
+
+bool TestMaxOfIndex()
+{
+  throw std::runtime_error("TestMaxOfIndex() Not yet implemented!");
+  return false;
+}
+
+bool TestMin()
+{
+  throw std::runtime_error("TestMin() Not yet implemented!");
+  return false;
+}
+
+bool TestMax()
+{
+  throw std::runtime_error("TestMax() Not yet implemented!");
+  return false;
+}
+
+bool TestVectorMedian()
+{
+  throw std::runtime_error("TestVectorMedian() Not yet implemented!");
+  return false;
+}
+
+bool TestConvertFrom()
+{
+  throw std::runtime_error("TestConvertFrom() Not yet implemented!");
+  return false;
+}
+
+bool TestSum()
+{
+  throw std::runtime_error("TestSum() Not yet implemented!");
+  return false;
+}
+
+bool TestVectorSumOfAbsoluteDifferences()
+{
+  throw std::runtime_error("TestVectorSumOfAbsoluteDifferences() Not yet implemented!");
+  return false;
+}
+
+bool TestOutputNode()
+{
+  throw std::runtime_error("TestOutputNode() Not yet implemented!");
+  return false;
+}
+
+bool TestWriteVectorToFile()
+{
+  throw std::runtime_error("TestWriteVectorToFile() Not yet implemented!");
+  return false;
+}
+
+bool TestWriteVectorToFileLines()
+{
+  throw std::runtime_error("TestWriteVectorToFileLines() Not yet implemented!");
+  return false;
+}
+
+bool TestOutputFirst()
+{
+  throw std::runtime_error("TestOutputFirst() Not yet implemented!");
+  return false;
+}
+
+bool TestExtractFirst()
+{
+  throw std::runtime_error("TestExtractFirst() Not yet implemented!");
+  return false;
+}
+
+bool TestContains()
+{
+  throw std::runtime_error("TestContains() Not yet implemented!");
+  return false;
+}
+
+bool TestOutput()
+{
+  throw std::runtime_error("TestOutput() Not yet implemented!");
+  return false;
+}
+
+bool TestClosestIndex()
+{
+  throw std::runtime_error("TestClosestIndex() Not yet implemented!");
+  return false;
+}
+
+bool TestIsNaN()
+{
+  throw std::runtime_error("TestIsNaN() Not yet implemented!");
+  return false;
+}
+
+bool TestContainsNaN()
+{
+  throw std::runtime_error("TestContainsNaN() Not yet implemented!");
+  return false;
+}
+
+bool TestKeepTopN()
+{
+  throw std::runtime_error("TestKeepTopN() Not yet implemented!");
+  return false;
+}
+
+bool TestDoesQueueContain()
+{
+  throw std::runtime_error("TestDoesQueueContain() Not yet implemented!");
+  return false;
+}
+
+bool TestDoesStackContain()
+{
+  throw std::runtime_error("TestDoesStackContain() Not yet implemented!");
+  return false;
+}
+
+bool TestForce0to255()
+{
+  throw std::runtime_error("TestForce0to255() Not yet implemented!");
+  return false;
+}
+
+bool TestHSV_H_Difference()
+{
+  throw std::runtime_error("TestHSV_H_Difference() Not yet implemented!");
+  return false;
+}
+
+bool TestFuzzyCompare()
+{
+  if(!Helpers::FuzzyCompare(1.0, 1.0))
+  {
+    return false;
+  }
+
+  return true;
 }
